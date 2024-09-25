2cc4eeaf9d13d133b3c72d69ccff502274b2e952
Soumya Ranjan Dalai <soumyaranjandalai429@gmail.com>
2024-09-25 08:13:59 +0530

    Change Background color by buttom click

--- MyPratice/App.js
+++ MyPratice/App.js
@@ -1,18 +1,59 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Button, Text } from "react-native";
+import { View, StyleSheet, Button, Text, TextInput, TouchableOpacity } from "react-native";
 
 const App = () => {
-    const [text, setText] = useState("Ram");
+    const [text, setText] = useState("");
+    const [display, setDisplay] = useState(false);
+    const [color, setColor] = useState("#fff");
+    const hide = () => {
+        setDisplay(false);
+    }
+    const clear = () => {
+        setText("");
+    }
+    const Color = () => {
+        if (color == "#fff") {
+            setColor("#000");
+        }
+        else {
+            setColor("#fff")
+        }
+    }
+    const textColor = color === "#fff" ? "#000" : "#fff";
 
     return (
-        <View style={{ marginTop: 200 }}>
-            <Text>{text}</Text>
-            <Button title="Click" onPress={() => setText("Sita")} />
+        <View style={[styles.Conatiner, { backgroundColor: color }]}
 
+
+        >
+            <TouchableOpacity onPress={Color} style={[{ borderWidth: 1, height: 30, width: 50 }]}>
+                <Text style={[{ color: textColor }]}>xyz</Text>
+            </TouchableOpacity>
+            <TextInput placeholder="Enter your name"
+                style={[{ color: textColor }]}
+                onChangeText={(text) => setText(text)}
+                value={text}
+            />
+            <Button title="Display" onPress={() => setDisplay(true)} />
+            <Button title="Hide" onPress={hide} />
+            <Button title="Clear" onPress={clear} />
+            {
+                display ? <View>
+                    <Text style={[{ color: textColor }]}>
+                        {text}
+                    </Text>
+
+                </View> : null
+            }
         </View>
     );
 }
 export default App;
 const styles = StyleSheet.create({
+    Conatiner: {
+        flex: 1,
+        marginTop: 200,
 
+
+    }
 })
